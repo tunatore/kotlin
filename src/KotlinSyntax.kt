e927832b911ed3820779d495880b977743e60ee8
tuna <tunatore@gmail.com>
2020-05-17 11:38:46 +0200

    kotlin object

--- src/KotlinSyntax.kt
+++ src/KotlinSyntax.kt
@@ -23,6 +23,19 @@ fun whenAssign(obj: Any): Any {
     return w
 }
 
+enum class Test {
+    TEST1,
+    TEST2,
+    TEST3
+}
+
+//object declaration -> cannot assign as expression ->lazy initialization
+object ObjectDeclaration {
+    fun objectMethod(test1: String, test2: String){
+        println("parameters = $test1:$test2")
+    }
+}
+
 fun main(){
     whenCases(1)
     whenCases(2)
@@ -30,4 +43,25 @@ fun main(){
     whenCases("test")
     println(whenAssign(1))
     println(whenAssign(2))
+
+    //enum
+    val testEnum = Test.TEST1
+    val testEnumWhen = when(testEnum) {
+        Test.TEST1 -> "TEST1"
+        Test.TEST2 -> "TEST2"
+        else -> "Unknown"
+    }
+    print(testEnumWhen)
+
+    // object expression -> created as anonymous class - lazy initialization
+    val objectExpression = object {                                                     //2
+        var value1: Int = 1
+        var value2: Int = 2
+    }
+
+    println(objectExpression.value1)
+    println(objectExpression.value2)
+
+    ObjectDeclaration.objectMethod("test1-value", "test2-value")
+
 }
